9450cda9b4128d8adf3432bed603c78a183ec1a7
heath-steiger <heathsteiger@gmail.com>
2025-02-25 10:11:48 -0500

    init commit

---
+++ Controllers/LineItemsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrsNetWeb.Models;
+
+namespace PrsNetWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineItemsController : ControllerBase
+    {
+        private readonly PRSContext _context;
+
+        public LineItemsController(PRSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LineItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LineItem>>> GetLineItems()
+        {
+            var lineItems = _context.LineItems.Include(l => l.Product)
+                                               .Include(l => l.Request);
+            return await lineItems.ToListAsync();
+        }
+
+        // GET: api/LineItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LineItem>> GetLineItem(int id)
+        {
+            var lineItem = await _context.LineItems.Include(l => l.Product)
+                                                   .Include(l => l.Request)
+                                                   .FirstOrDefaultAsync(l => l.Id == id);
+            if (lineItem == null)
+            {
+                return NotFound();
+            }
+
+            return lineItem;
+        }
+
+        // PUT: api/LineItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLineItem(int id, LineItem lineItem)
+        {
+            if (id != lineItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lineItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LineItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LineItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<LineItem>> PostLineItem(LineItem lineItem)
+        {
+            _context.LineItems.Add(lineItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLineItem", new { id = lineItem.Id }, lineItem);
+        }
+
+        // DELETE: api/LineItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLineItem(int id)
+        {
+            var lineItem = await _context.LineItems.FindAsync(id);
+            if (lineItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.LineItems.Remove(lineItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LineItemExists(int id)
+        {
+            return _context.LineItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/LineItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrsNetWeb.Models;
+
+[Table("LineItem")]
+[Index("RequestId", "ProductId", Name = "UC_req_pdt", IsUnique = true)]
+public partial class LineItem
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("RequestID")]
+    public int RequestId { get; set; }
+
+    [Column("ProductID")]
+    public int ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    [ForeignKey("ProductId")]
+    [InverseProperty("LineItems")]
+    public  Product? Product { get; set; } = null!;
+
+    //[ForeignKey("RequestId")]
+    //[InverseProperty("LineItems")]
+    public Request? Request { get; set; } = null!;
+}
